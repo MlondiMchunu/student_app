0336a064a711f9f199dd2ab1722f8319add1a6b4
Stopa <sikhuliso@gmail.com>
2024-05-30 16:13:18 +0200

    encapsulation

--- tables/classes/student.class.php
+++ tables/classes/student.class.php
@@ -20,5 +20,33 @@ class Student extends MySQLDatabase{
     public function setName($name){
         $this->name = $name;
     }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setAge($age){
+        $this->age = $age;
+    }
+
+    public function getAge(){
+        return $this->age;
+    }
+
+    public function setMajor($major){
+        $this->major = $major;
+    }
+
+    public function getMajor(){
+        return $this->major;
+    }
 }
 ?>
