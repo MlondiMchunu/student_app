8ef080ac70f682fbfc4bd6d954a7148e5bd0bd48
Stopa <sikhuliso@gmail.com>
2024-05-30 20:45:53 +0200

    js commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function editStudent(id, name, age, level) {
+    document.getElementById('id').value = id;
+    document.getElementById('name').value = name;
+    document.getElementById('age').value = age;
+    document.getElementById('level').value = level;
+}
