8efcab74c032c2f02c37ed65768609aba6f6752b
Stopa <sikhuliso@gmail.com>
2024-05-30 16:06:29 +0200

    set method

--- tables/classes/student.class.php
+++ tables/classes/student.class.php
@@ -6,6 +6,19 @@ class Student extends MySQLDatabase{
     private $age;
     private $major;
 
-  
+    public function __construct($id = null){
+        parent::__construct();
+
+        if($id){
+            $this->id = $id;
+            $this->read();
+        }
+    }
+
+    //Encapsulation: Setters and Getters
+
+    public function setName($name){
+        $this->name = $name;
+    }
 }
 ?>
